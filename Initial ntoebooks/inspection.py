26f346b4622ff38f7c5a3b98966c96f926469e3e
RasNoergaard <s225786@student.dtu.dk>
2024-11-04 15:13:18 +0100

    Checkpoint: finished making PCA plot, feature plot and function for all patients individually

---
+++ Initial ntoebooks/inspection.py
@@ -0,0 +1,35 @@
+import mne
+import os
+
+# ----------------------------
+# Assumption: UNPROCESSED DATA
+
+#path = 'EDF filer'
+#folder = 'L:\\LovbeskyttetMapper\\CONNECT-ME\\cEEG projekt - CONNECT-ME\\EEGer\\Anonymiserede EEGer\\'
+#full_path = os.path.join(folder, path)
+#raw = mne.io.read_raw_edf(full_path + '\\10GK-EDF+1.edf', preload=True)
+
+#plot
+#raw.plot(block = True)
+#print(raw.info)
+
+#spectrum = raw.compute_psd()
+#spectrum.plot(average=True, picks="data", exclude="bads", amplitude=False)
+
+
+# ----------------------------
+
+
+# Assumption: PREPROCESSED DATA
+path = 'used_raws'
+folder = 'L:\\LovbeskyttetMapper\\CONNECT-ME\\cEEG projekt - CONNECT-ME\\Mappe til Cecilie og Katharina\\nice\\examples\\'
+full_path = os.path.join(folder, path)
+raw = mne.io.read_raw_fif(full_path + '\\patient10_raw.fif', preload=True)
+
+#plot
+raw.plot(block = True)
+print(raw.info)
+
+#spectrum = raw.compute_psd()
+#spectrum.plot(average=True, picks="data", exclude="bads", amplitude=False)
+
