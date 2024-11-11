cbfe33253bb119d97a0b0b27fc5c6a75cacd61cc
RasNoergaard <s225786@student.dtu.dk>
2024-11-11 19:52:08 +0100

    Classification implement

--- Initial ntoebooks/inspection.py
+++ Initial ntoebooks/inspection.py
@@ -24,7 +24,7 @@ import os
 path = 'used_raws'
 folder = 'L:\\LovbeskyttetMapper\\CONNECT-ME\\cEEG projekt - CONNECT-ME\\Mappe til Cecilie og Katharina\\nice\\examples\\'
 full_path = os.path.join(folder, path)
-raw = mne.io.read_raw_fif(full_path + '\\patient10_raw.fif', preload=True)
+raw = mne.io.read_raw_fif(full_path + '\\patient2_raw.fif', preload=True)
 
 #plot
 raw.plot(block = True)
